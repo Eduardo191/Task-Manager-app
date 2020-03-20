86e563203e8e3ec1b4298ae13e599ac9a5a2cb4a
Eduardo <feduardo191@gmail.com>
2020-03-20 20:35:58 -0300

    Refatorando funções assíncronas da api

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,24 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5e617577d3a42d43f4f61cca').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
+// Task.findByIdAndDelete('5e617577d3a42d43f4f61cca').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5e615ae71466120fb051c80f').then((result) => {
     console.log(result)
 }).catch((e) => {
     console.log(e)
 })
+
