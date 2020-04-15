eb278d0b9d93012a84d90537553001aa197f34bf
Eduardo <feduardo191@gmail.com>
2020-04-15 19:53:34 -0300

    Habilitando timestamps para users e tasks

--- src/models/task.js
+++ src/models/task.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
         trim: true
-    }, 
+    },
     completed: {
         type: Boolean,
         required: false,
@@ -16,6 +16,10 @@ const Task = mongoose.model('Task', {
         required: true,
         ref: 'User'
     }
+}, {
+    timestamps: true
 })
+
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
